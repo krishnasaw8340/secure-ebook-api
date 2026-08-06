6d0a94d2049b157320dbf125032b6922d4ff31b7
Krishna Kr Saw <krishnakrsaw8340@gamil.com>
2026-08-06 18:46:10 +0530

    module-created-for-each-section

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { CatalogModule } from './catalog/catalog.module';
+import { ReadingModule } from './reading/reading.module';
+import { WalletModule } from './wallet/wallet.module';
+import { PaymentModule } from './payment/payment.module';
+import { CommonModule } from './common/common.module';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, CatalogModule, ReadingModule, WalletModule, PaymentModule, CommonModule, DatabaseModule, ConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
