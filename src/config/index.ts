3c5f8416979351be88bf43945e9850a78c2cd44f
Krishna Kr Saw <krishnakrsaw8340@gamil.com>
2026-08-07 09:19:13 +0530

    updated-joi-setup-validation

---
+++ src/config/index.ts
@@ -0,0 +1,13 @@
+import appConfig from './app.config';
+import databaseConfig from './database.config';
+import jwtConfig from './jwt.config';
+import mailConfig from './mail.config';
+import paymentConfig from './payment.config';
+
+export default [
+  appConfig,
+  databaseConfig,
+  jwtConfig,
+  mailConfig,
+  paymentConfig,
+];
